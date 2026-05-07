62ad8cf96f08cc43176594bf8f5f2acba9b16047
olafkirs <olafa@tlu.ee>
2026-05-07 19:42:04 +0300

    1

---
+++ veebipood/veebipood/src/test/java/ee/olaf/veebipood/VeebipoodApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.olaf.veebipood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeebipoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
