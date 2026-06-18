8379bace556db8c43301faf8099ff96916358107
olafkirs <olafa@tlu.ee>
2026-06-18 10:17:20 +0300

    uus

---
+++ 14_15.05-kontrolltoo2/autod/autod/src/test/java/ee/olaf/autod/AutodApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.olaf.autod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
